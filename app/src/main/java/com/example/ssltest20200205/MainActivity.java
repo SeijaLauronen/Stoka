547f3fad06bf2f0ace78e9e39ccb80025e26c336
SeijaLauronen <seija.lauronen@gmail.com>
2020-02-05 15:37:59 +0200

    Kokeillaan mimten tulee GitHubiin toka projekti

---
+++ app/src/main/java/com/example/ssltest20200205/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ssltest20200205;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Tässä ei vielä ole yhtään mitään
+    }
+}
